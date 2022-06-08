3424e926ffcb0f1f91b4163af32c125ca6ea0310
Ford Powers <ford.powers@pnnl.gov>
2022-06-07 21:14:21 -0700

    optimize donate.js images

--- pages/donate.js
+++ pages/donate.js
@@ -1,8 +1,12 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Button, Heading, Image, Text } from '@chakra-ui/react';
+import { Button, Heading, Text } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
 import Layout from '../components/Layout';
+import Image from 'next/image';
+
+import paypal from '../public/paypal.webp'
+import amazon from '../public/amazon.webp'
 
 export default function Donate() {
   return (
@@ -16,11 +20,15 @@ export default function Donate() {
       <Heading size='md'>Make a tax-deductible donation to PISCES</Heading>
       <Text variant='content'>For large or recurring donations please email info@pisces-intl.org</Text>
       <Link href="https://www.paypal.com/donate?business=VTZ5DAMVPJ42S&item_name=Donate+to+PISCES&currency_code=USD" passHref>
-        <Button size='lg' mt='1em' mb='2em' rightIcon={<ExternalLinkIcon />}><Image alt='Donate with Paypal' w='6em' src='/paypal.webp' /></Button>
+        <Button maxW='12em' size='lg' mt='1em' mb='2em' rightIcon={<ExternalLinkIcon />}>
+          <Image alt='Donate with Paypal' src={paypal} />
+        </Button>
       </Link>
       <Text variant='content'>You can shop on Amazon Smile with the below URL to share a percentage of the purchase with us.</Text>
       <Link href="https://smile.amazon.com/ch/84-2548117" passHref>
-        <Button size='lg' mt='1em' mb='5em' rightIcon={<ExternalLinkIcon />}><Image alt='Amazon Smile' w='6em' src='/amazon.webp' /></Button>
+        <Button maxW='12em' size='lg' mt='1em' mb='5em' rightIcon={<ExternalLinkIcon />}>
+          <Image alt='Amazon Smile' src={amazon} />
+        </Button>
       </Link>
     </Layout>
   );
