9617a67eeaf9079c406f4eb42a590f6e7ac5e16d
Ford Powers <ford.powers@pnnl.gov>
2022-06-09 08:18:14 -0700

    static image optimization workaround

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withExportImages = require('next-export-optimize-images')
 
-module.exports = nextConfig
+module.exports = withExportImages({
+  reactStrictMode: true,
+})
