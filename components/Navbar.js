61763d829542a047ccb5ae88bceb5f46bd8a7164
Ford Powers <ford.powers@pnnl.gov>
2023-04-05 13:42:19 -0700

    add cisse page

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,6 +51,7 @@ export default function Navbar() {
             <MenuGroup>
               <Link href="/partners/community" passHref><MenuItem>Community Partners FAQ</MenuItem></Link>
               <Link href="/partners/academic" passHref><MenuItem>Academic Partners FAQ</MenuItem></Link>
+              <Link href="/partners/cisse" passHref><MenuItem>CISSE</MenuItem></Link>
               <Link href="/partners/current-partners" passHref><MenuItem>Current Partners</MenuItem></Link>
             </MenuGroup>
             <MenuDivider />
@@ -87,6 +88,7 @@ export default function Navbar() {
             <MenuList>
               <Link href="/partners/community" passHref><MenuItem>Community Partners FAQ</MenuItem></Link>
               <Link href="/partners/academic" passHref><MenuItem>Academic Partners FAQ</MenuItem></Link>
+              <Link href="/partners/cisse" passHref><MenuItem>CISSE</MenuItem></Link>
               <Link href="/partners/current-partners" passHref><MenuItem>Current Partners</MenuItem></Link>
             </MenuList>
           </Menu>
