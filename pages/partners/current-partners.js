4252ada46de36991113ccdde9db8526883240d08
Ford Powers <ford.powers@pnnl.gov>
2023-11-06 10:08:10 -0800

    update dependencies

--- pages/partners/current-partners.js
+++ pages/partners/current-partners.js
@@ -16,7 +16,7 @@ export default function CurrentPartners() {
       <Heading textAlign='left' size='lg'>Our Partners</Heading>
       <Wrap spacing='1em' pt='1.5em' mb='2em'>
         {data.map((partner, index) =>
-          <SlideFade in={true} transition={{ enter: { delay: 0.125 * (index + 1) } }}>
+          <SlideFade key={partner.image} in={true} transition={{ enter: { delay: 0.125 * (index + 1) } }}>
             <PartnerCard isMobile={isMobile} key={partner.image} partner={partner} />
           </SlideFade>
         )}
