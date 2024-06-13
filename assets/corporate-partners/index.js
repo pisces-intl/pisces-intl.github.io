12c230c26b88ee4a70145048c05a320aa598f962
fordepowers <46435250+fordepowers@users.noreply.github.com>
2024-06-13 01:51:14 +0000

    Deploying to gh-pages from @ pisces-intl/pisces-intl.github.io@33bfc3ff5a7c7593a49a33e9a766660407ad8b90 🚀

---
+++ assets/corporate-partners/index.js
@@ -0,0 +1,6 @@
+export * as ci from './ci.webp';
+export * as ci_mobile from './ci-mobile.webp';
+export * as ii from './ii.webp';
+export * as ii_mobile from './ii-mobile.webp';
+export * as ol from './ol.webp';
+export * as ol_mobile from './ol-mobile.webp';
