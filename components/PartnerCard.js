99d6b014acef12a5af97c1a25186b887297b85cc
Beau Jones <beaujones@student.olympic.edu>
2024-10-10 01:54:15 -0700

    mobile image fix

--- components/PartnerCard.js
+++ components/PartnerCard.js
@@ -39,6 +39,7 @@ function PartnerCard(props) {
           borderRadius={isMobile ? '0.5em' : '1em'}
         >
           <Image
+            unoptimized
             priority={true}
             layout='fill'
             placeholder='blur'
